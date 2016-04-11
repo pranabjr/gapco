d35ae039e428c1d1b8b6660d4cf2cd394cf242a2
Pranab Jung Rana <pranabjr@gmail.com>
2016-04-11 15:35:32 +0200

    changes made

--- js/scroll.js
+++ js/scroll.js
@@ -143,5 +143,9 @@ $(function() {
 
 })(jQuery);
 
-// for refreshing the page
+//auto expand textarea
+function adjust_textarea(h) {
+    h.style.height = "20px";
+    h.style.height = (h.scrollHeight)+"px";
+}
 
