4d712063d0d7d23fdb968399c304b3431b5d2448
Pranab Jung Rana <pranabjr@gmail.com>
2016-04-11 18:29:05 +0200

    footer added

--- js/scroll.js
+++ js/scroll.js
@@ -149,3 +149,15 @@ function adjust_textarea(h) {
     h.style.height = (h.scrollHeight)+"px";
 }
 
+
+//for map
+
+function initialize() {
+  var mapProp = {
+    center:new google.maps.LatLng(27.7172,85.3240),
+    zoom:5,
+    mapTypeId:google.maps.MapTypeId.ROADMAP
+  };
+  var map=new google.maps.Map(document.getElementById("googleMap"), mapProp);
+}
+google.maps.event.addDomListener(window, 'load', initialize);
